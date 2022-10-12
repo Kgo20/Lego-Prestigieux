e90636c075d8cd1f49f57eef15f15dfc2e8c4d2c
Kgo20 <kgo201807@gmail.com>
2022-10-11 21:23:02 -0400

    Changement demander par le prof lors de la correction du tp1

---
+++ Lego_Prestigieux/ViewComponents/Contacts.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Lego_Prestigieux.ViewComponents
+{
+    public class Contacts : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("~/Views/Shared/Components/Contact/Contact.cshtml");
+        }
+    }
+}
