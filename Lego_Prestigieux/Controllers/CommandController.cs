e3987d0092a008e0d8bf8625ecc03a52a32e277d
RaphS <89414388+RaphS@users.noreply.github.com>
2022-11-06 15:38:46 -0500

    Retirer Commande

--- Lego_Prestigieux/Controllers/CommandController.cs
+++ Lego_Prestigieux/Controllers/CommandController.cs
@@ -25,6 +25,43 @@ namespace Lego_Prestigieux.Controllers
 
         }
 
+        public async Task<IActionResult> Details(int commandId)
+        {
+            return NotFound();
+        }
+
+        public async Task<IActionResult> Remove(int commandId)
+        {
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                if (userId == null)
+                    return RedirectToAction("Login", "Account", null);
+
+                CommandModel command = await _context.Commands.Include("Products").FirstAsync(c=>c.Id == commandId);
+
+                if (!User.IsInRole("Admin") && command.UserId != userId)
+                {
+                    return RedirectToAction("List");
+                }
+
+                foreach (var item in command.Products)
+                {
+                    item.CommandModel = null;
+                }
+
+                command.Status = CommandStatus.Canceled;
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("List");
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
+        }
+
         public async Task<IActionResult> List()
         {
             try
@@ -38,14 +75,14 @@ namespace Lego_Prestigieux.Controllers
 
                 if (User.IsInRole("Admin"))
                 {
-                    commands = await _context.Commands.ToListAsync();
+                    commands = await _context.Commands.OrderBy(c=>c.Status).ToListAsync();
                 }
                 else
                 {
-                    commands = await _context.Commands.Where(c => c.UserId == userId).ToListAsync();
+                    commands = await _context.Commands.OrderBy(c => c.Status).Where(c => c.UserId == userId).ToListAsync();
                 }
 
-                return View("Command", commands);
+                return View("Commands", commands);
             }
             catch (Exception e)
             {
--- Lego_Prestigieux/Models/CommandInfo.cs
+++ Lego_Prestigieux/Models/CommandInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lego_Prestigieux.Models
 {
