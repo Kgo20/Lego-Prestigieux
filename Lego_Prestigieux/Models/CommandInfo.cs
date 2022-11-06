14764f1c2c7b232d3499bf6fbecb2a9ef3dd4afb
RaphS <89414388+RaphS@users.noreply.github.com>
2022-11-06 01:18:41 -0400

    Commandes

---
+++ Lego_Prestigieux/Models/CommandInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lego_Prestigieux.Models
+{
+    public class CommandInfo
+    {
+        public float CommandTotal { get; set; }
+        public int ProductAmount { get; set; }
+        public DateTime CommandCreationDate { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        public CommandStatus Status { get; set; }
+
+        public int CommandId { get; set; }
+    }
+}
---
+++ Lego_Prestigieux/ViewComponents/Command.cs
@@ -0,0 +1,37 @@
+﻿using Lego_Prestigieux.Data;
+using Lego_Prestigieux.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Lego_Prestigieux.ViewComponents
+{
+    public class Command : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Command(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            CommandModel Command = _context.Commands.Where(c => c.Id == id).FirstOrDefault();
+
+            var CommandVM = new CommandInfo
+            {
+                CommandId = Command.Id,
+                CommandCreationDate = DateTime.Now,
+                ExpectedDeliveryDate = DateTime.Now.AddDays(14),
+                CommandTotal = Command.Products.Sum(p => p.PriceUnit * p.Quantity),
+                ProductAmount = Command.Products.Count(),
+                Status = Command.Status
+            };
+
+            return View("Command", CommandVM);
+        }
+    }
+}
